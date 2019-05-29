737b28379c37178b4d84ea313cdb59261a50d49d
N3wika <misa.arnostova@seznam.cz>
2019-05-29 22:12:30 +0200

    Umi lepit mezery pred text (zarovnava vpravo)... a java vi proc Eclipsa nadava...

--- src/cz/alisma/alej/text/wrapping/RightAligner.java
+++ src/cz/alisma/alej/text/wrapping/RightAligner.java
@@ -1,13 +1,36 @@
+/*
+ * MIT License
+ * Copyright (c) 2017 Gymnazium Nad Aleji
+ * Copyright (c) 2017 Vojtech Horky
+ * Copyright (c) 2019 Vojtech Travnicek, Michaela Arnostova
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package cz.alisma.alej.text.wrapping;
 
 import java.util.List;
 
 /** Right-aligns a line. */
-
 public class RightAligner implements Aligner {
-	@Override
-    public String format(List<String> words) {
-		//upravit pro RIGHT!!
+
+    @Override
+    public String format(List<String> words, int delkaRadek) {
         StringBuilder result = new StringBuilder();
         
         boolean first = true;
@@ -20,6 +43,17 @@ public class RightAligner implements Aligner {
             result.append(w);
         }
         
+        //lepeni mezer na zacatek
+        
+        String mezeryKNalepeni = "";
+        if (result.length() < delkaRadek) {
+            
+            while (result.length() + mezeryKNalepeni.length() < delkaRadek) {
+                mezeryKNalepeni += " ";
+            }
+            result.insert( 0, mezeryKNalepeni );
+        }
         return result.toString();
     }
+
 }

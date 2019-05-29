7a8fef60a598cd4a11b5b80afcd7a5cf21a36569
N3wika <misa.arnostova@seznam.cz>
2019-05-29 22:39:29 +0200

    Kopiruje RightAligner - nalepovacich mezer je pulka :)

--- src/cz/alisma/alej/text/wrapping/CenterAligner.java
+++ src/cz/alisma/alej/text/wrapping/CenterAligner.java
@@ -1,6 +1,64 @@
+/*
+ * MIT License
+ * Copyright (c) 2017 Gymnazium Nad Aleji
+ * Copyright (c) 2017 Vojtech Horky
+ * Copyright (c) 2019 Vojtech Travnicek, Michaela Arnostova
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package cz.alisma.alej.text.wrapping;
 
+import java.util.List;
 
-public class CenterAligner {
+/** Center-aligns a line. */
+public class CenterAligner implements Aligner {
+
+    //prakticky shodne s rightAligner
+    @Override
+    public String format( List<String> words, int delkaRadek ) {
+        StringBuilder result = new StringBuilder();
+
+        boolean first = true;
+        for ( String w : words ) {
+            if ( !first ) {
+                result.append( " " );
+            } else {
+                first = false;
+            }
+            result.append( w );
+        }
+
+        //lepeni mezer na zacatek
+        //aby byly mezery pred i za textem tak se lepi jen kazda druha
+
+        String mezeryKNalepeni = "";
+        if ( result.length() < delkaRadek ) {
+            int pruchod = 0;
+            while ( result.length() + mezeryKNalepeni.length() < delkaRadek ) {
+                if ( pruchod % 2 == 0 ) {
+                    mezeryKNalepeni += " ";
+                }
+                pruchod++;
+            }
+            result.insert( 0, mezeryKNalepeni );
+        }
+        return result.toString();
+    }
 
 }

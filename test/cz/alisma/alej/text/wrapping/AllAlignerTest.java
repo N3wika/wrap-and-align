63461f58222e94171835addda017e8b3cf1f24f1
N3wika <misa.arnostova@seznam.cz>
2019-05-31 23:32:02 +0200

    Test pro left a right, lze pouzit jako vzor pro dalsi

---
+++ test/cz/alisma/alej/text/wrapping/AllAlignerTest.java
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ * Copyright (c) 2017 Gymnazium Nad Aleji
+ * Copyright (c) 2019 Vojtech Travnicek, Michaela Arnostova
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package cz.alisma.alej.text.wrapping;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class AllAlignerTest {
+
+    @Test
+    public void leftAlignerTest() {
+        Aligner aligner = new LeftAligner();
+        List<String> words = new ArrayList<String>();
+        words.add( "hello" );
+        words.add( "world" );
+        String result = aligner.format( words , 16 );
+        assertEquals("Whoa", "hello world", result);
+    }
+    
+    @Test
+    public void rightAlignerTest() {
+        Aligner aligner = new RightAligner();
+        List<String> words = new ArrayList<String>();
+        words.add( "hello" );
+        words.add( "world" );
+        String result = aligner.format( words , 16 );
+        assertEquals("Whoa", "     hello world", result);
+    }
+}
